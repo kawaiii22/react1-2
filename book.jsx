ec066c01e968e8b778329fb51f09b19cf73dc68c
kawaiii22 <kdydy2085@gmail.com>
2024-03-27 14:57:06 +0900

    main

---
+++ book.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function book(props){
+    return(
+        <div>
+            <h3>{'이 책의 이름은 $(props.name)입니다.'}</h3>
+            <h3>{'이 책의 이름은 $(props.name)입니다.'}</h3>
+
+        </div>
+
+    );
+}
