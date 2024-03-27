b28e2c2312faef04a749a4db5a54882d4a1dae26
kawaiii22 <kdydy2085@gmail.com>
2024-03-27 15:45:01 +0900

    main

--- book.jsx
+++ book.jsx
@@ -6,7 +6,19 @@ export default function book(props){
             <h3>{'이 책의 이름은 $(props.name)입니다.'}</h3>
             <h3>{'이 책의 이름은 $(props.name)입니다.'}</h3>
 
+
         </div>
 
     );
+
 }
+export default function Clock(props){
+    return(
+        <div>
+            <h1>
+                현재시간: {new Date().toLocaleTimeString()}
+            </h1>
+        </div>
+    )
+}
+expor
